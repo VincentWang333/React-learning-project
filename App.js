9dec59c1879cc24947bd06c78864de8ba792d164
vincentwang333 <vincentwang333@outlook.com>
2020-06-08 11:20:23 -0400

    initial push

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {createAppContainer } from 'react-navigation';
+import {createStackNavigator } from 'react-navigation-stack';
+import {Provider} from './src/context/BlogContext';
+import ShowScreen from './src/screen/ShowScreen';
+import IndexScreen from './src/screen/IndexScreen';
+import CreateScreen from './src/screen/CreateScreen';
+import EditScreen from './src/screen/EditScreen';
+
+const navigator = createStackNavigator({
+  Index: IndexScreen,
+  Show: ShowScreen,
+  Create: CreateScreen,
+  Edit: EditScreen
+},{
+  initialRouteName: 'Index',
+  defaultNavigationOptions:{
+    title: "Blog"
+  }
+});
+
+
+const App = createAppContainer(navigator);
+
+export default() => {
+  return <Provider>
+    <App />
+  </Provider>
+};
---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://fff74f72.ngrok.io'
+});
